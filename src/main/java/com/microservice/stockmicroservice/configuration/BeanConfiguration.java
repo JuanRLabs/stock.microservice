7cfe762ebb298da9659fd4e1161ad6e8d2e1ee3f
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-30 00:39:16 -0500

    agregando beans de puertos de servicio y persistencia brand

--- src/main/java/com/microservice/stockmicroservice/configuration/BeanConfiguration.java
+++ src/main/java/com/microservice/stockmicroservice/configuration/BeanConfiguration.java
@@ -1,10 +1,16 @@
 package com.microservice.stockmicroservice.configuration;
 
+import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.adapter.BrandAdapter;
 import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.adapter.CategoryAdapter;
+import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.mapper.IBrandEntityMapper;
 import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.mapper.ICategoryEntityMapper;
+import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.repository.IBrandRepository;
 import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.repository.ICategoryRepository;
+import com.microservice.stockmicroservice.domain.api.IBrandServicePort;
 import com.microservice.stockmicroservice.domain.api.ICategoryServicePort;
+import com.microservice.stockmicroservice.domain.api.usecase.BrandUseCase;
 import com.microservice.stockmicroservice.domain.api.usecase.CategoryUseCase;
+import com.microservice.stockmicroservice.domain.spi.Brand.IBrandPersistencePort;
 import com.microservice.stockmicroservice.domain.spi.category.ICategoryPersistencePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +21,8 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
     private final ICategoryRepository categoryRepository;
     private final ICategoryEntityMapper categoryEntityMapper;
+    private final IBrandRepository brandRepository;
+    private final IBrandEntityMapper brandEntityMapper;
 
     @Bean
     public ICategoryPersistencePort categoryPersistencePort(){
@@ -25,5 +33,17 @@ public class BeanConfiguration {
     public ICategoryServicePort categoryServicePort(){
         return new CategoryUseCase(categoryPersistencePort());
     }
+
+    // Brand beans
+    @Bean
+    public IBrandPersistencePort brandPersistencePort(){
+        return new BrandAdapter(brandRepository, brandEntityMapper);
+    }
+
+    @Bean
+    public IBrandServicePort brandServicePort(){
+        return new BrandUseCase(brandPersistencePort());
+    }
+
 
 }
