d4cf206a7fb05d8db333f5faf4d8898218cfc625
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-30 01:19:51 -0500

    agregando mapping de propiedades

--- src/main/java/com/microservice/stockmicroservice/adapters/driven/jpa/mysql/mapper/IBrandEntityMapper.java
+++ src/main/java/com/microservice/stockmicroservice/adapters/driven/jpa/mysql/mapper/IBrandEntityMapper.java
@@ -3,10 +3,18 @@ package com.microservice.stockmicroservice.adapters.driven.jpa.mysql.mapper;
 import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.entity.BrandEntity;
 import com.microservice.stockmicroservice.domain.model.Brand;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface IBrandEntityMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
     BrandEntity toEntity(Brand brand);
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
     Brand toModel(BrandEntity brandEntity);
 }
