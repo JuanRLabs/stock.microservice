022335b8437c95939acfbd7eb7c9792f15b67a4a
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-30 00:32:39 -0500

    creacion de adapter Controller de brand

---
+++ src/main/java/com/microservice/stockmicroservice/adapters/driving/http/controller/BrandRestControllerAdapter.java
@@ -0,0 +1,26 @@
+package com.microservice.stockmicroservice.adapters.driving.http.controller;
+
+import com.microservice.stockmicroservice.adapters.driving.http.dto.request.AddBrandRequest;
+import com.microservice.stockmicroservice.adapters.driving.http.mapper.IBrandRequestMapper;
+import com.microservice.stockmicroservice.domain.api.IBrandServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("brand")
+@RequiredArgsConstructor
+public class BrandRestControllerAdapter {
+    private final IBrandServicePort brandServicePort;
+    private final IBrandRequestMapper brandRequestMapper;
+
+    @PostMapping("/")
+    public ResponseEntity<Void> createBrand(AddBrandRequest brandRequest){
+        brandServicePort.create(brandRequestMapper.addRequestToModel(brandRequest));
+        return ResponseEntity.status(HttpStatus.CREATED ).build();
+    }
+
+}
