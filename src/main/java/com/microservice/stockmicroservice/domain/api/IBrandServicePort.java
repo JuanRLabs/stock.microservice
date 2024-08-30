63a41ad2d957bc11dd25e3d417bf8805cf1397d7
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-29 22:48:10 -0500

    modificacion Nombre interface PersistencePort para brand

--- src/main/java/com/microservice/stockmicroservice/domain/api/IBrandServicePort.java
+++ src/main/java/com/microservice/stockmicroservice/domain/api/IBrandServicePort.java
@@ -2,6 +2,6 @@ package com.microservice.stockmicroservice.domain.api;
 
 import com.microservice.stockmicroservice.domain.model.Brand;
 
-public interface IBrandServiceport {
+public interface IBrandServicePort {
     void create(Brand brand);
 }
