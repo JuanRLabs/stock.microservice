6a52658f7a41c4b49484c7f82a8af276c6a710e6
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-30 00:16:29 -0500

    creacion de mapper brand de model a entity

---
+++ src/main/java/com/microservice/stockmicroservice/adapters/driven/jpa/mysql/mapper/IBrandEntityMapper.java
@@ -0,0 +1,11 @@
+package com.microservice.stockmicroservice.adapters.driven.jpa.mysql.mapper;
+
+import com.microservice.stockmicroservice.adapters.driven.jpa.mysql.entity.BrandEntity;
+import com.microservice.stockmicroservice.domain.model.Brand;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IBrandEntityMapper {
+    BrandEntity toEntity(Brand brand);
+}
