2aefbdcd1e51a18d4ee0ab1ba2130cecd54adb65
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-30 00:35:24 -0500

    modificacion de getter y setter sin lombok

--- src/main/java/com/microservice/stockmicroservice/adapters/driving/http/dto/request/AddBrandRequest.java
+++ src/main/java/com/microservice/stockmicroservice/adapters/driving/http/dto/request/AddBrandRequest.java
@@ -1,9 +1,22 @@
 package com.microservice.stockmicroservice.adapters.driving.http.dto.request;
 
-import lombok.Data;
-
-@Data
 public class AddBrandRequest {
     private String name;
     private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
