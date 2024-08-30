a6a8524d4dfb56d5335fe54a640050d115266482
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-29 22:43:59 -0500

    creacion interface servicePort para brand

---
+++ src/main/java/com/microservice/stockmicroservice/domain/api/IBrandServiceport.java
@@ -0,0 +1,7 @@
+package com.microservice.stockmicroservice.domain.api;
+
+import com.microservice.stockmicroservice.domain.model.Brand;
+
+public interface IBrandServiceport {
+    void create(Brand brand);
+}
