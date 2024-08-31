96d2ee16bbf6f11ce96ee1493ba5fe64d7e40ee7
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-31 02:03:03 -0500

    creando mapper de request to model product

---
+++ src/main/java/com/microservice/stockmicroservice/adapters/driving/http/mapper/IProductRequestMapper.java
@@ -0,0 +1,14 @@
+package com.microservice.stockmicroservice.adapters.driving.http.mapper;
+
+import com.microservice.stockmicroservice.adapters.driving.http.dto.request.AddProductRequest;
+import com.microservice.stockmicroservice.domain.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",  unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IProductRequestMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Product addRequestToProduct(AddProductRequest addProductRequest);
+}
