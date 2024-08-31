2e7fa4524e74fcb29b1619f9b0780bd5936aa447
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-08-31 02:04:12 -0500

    creando firma metodos para la agilidad de flujo de persistencia

--- src/main/java/com/microservice/stockmicroservice/domain/spi/product/IProductPersistencePort.java
+++ src/main/java/com/microservice/stockmicroservice/domain/spi/product/IProductPersistencePort.java
@@ -4,4 +4,7 @@ import com.microservice.stockmicroservice.domain.model.Product;
 
 public interface IProductPersistencePort {
     void create(Product product);
+    boolean existsBrand(Long id);
+    Product findByName(String name);
+
 }
