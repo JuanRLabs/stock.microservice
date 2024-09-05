1617aee5b4fc1655ef3eebd27ff7595f67d41ab6
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-05 07:47:56 -0500

    se elimina redundancia de clase para excepcion.

--- src/main/java/com/microservice/stockmicroservice/adapters/driving/http/controller/ProductsNotFoundToMapper.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.stockmicroservice.adapters.driving.http.controller;
-
-public class ProductsNotFoundToMapper extends RuntimeException {
-    public ProductsNotFoundToMapper(String message) {
-        super(message);
-    }
-}
