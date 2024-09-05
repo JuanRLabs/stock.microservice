096ffee086be45c44dd6ed0ee3de1caa144af8c3
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-05 07:43:54 -0500

    se crea clase de excepcion  generica .

--- src/main/java/com/microservice/stockmicroservice/adapters/driven/jpa/mysql/exception/CategoryNotFoundToMapper.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.stockmicroservice.adapters.driven.jpa.mysql.exception;
-
-public class CategoryNotFoundToMapper extends RuntimeException{
-    public CategoryNotFoundToMapper(String message) {
-        super(message);
-    }
-}
