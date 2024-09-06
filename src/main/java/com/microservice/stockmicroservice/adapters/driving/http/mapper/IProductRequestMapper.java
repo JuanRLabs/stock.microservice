3d88578c7fbc015ecff54fc76dbc2132926bb156
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-05 23:51:28 -0500

    se valida constructo modelo dominio por seteo

--- src/main/java/com/microservice/stockmicroservice/adapters/driving/http/mapper/IProductRequestMapper.java
+++ src/main/java/com/microservice/stockmicroservice/adapters/driving/http/mapper/IProductRequestMapper.java
@@ -1,40 +1,43 @@
 package com.microservice.stockmicroservice.adapters.driving.http.mapper;
 
 import com.microservice.stockmicroservice.adapters.driving.http.dto.request.AddProductRequest;
+import com.microservice.stockmicroservice.domain.exceptions.EmptyFieldException;
 import com.microservice.stockmicroservice.domain.model.Brand;
 import com.microservice.stockmicroservice.domain.model.Category;
 import com.microservice.stockmicroservice.domain.model.Product;
+import com.microservice.stockmicroservice.domain.util.DomainConstants;
 import org.mapstruct.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Mapper(componentModel = "spring",  unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface IProductRequestMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "name", target = "name")
-    @Mapping(source = "description", target = "description")
-    @Mapping(source = "quantity", target = "quantity")
-    @Mapping(source = "price", target = "price")
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "quantity", source = "quantity")
+    @Mapping(target = "price", source = "price")
     @Mapping(source = "brandId", target = "brand", qualifiedByName = "brandMapper")
     @Mapping(source = "categoriesId", target = "categoriesId", qualifiedByName = "CategoryMapper")
     Product addRequestToProduct(AddProductRequest addProductRequest);
 
     @Named("brandMapper")
     default Brand brandMapper(Long brandId) {
-        Brand brand = new Brand(brandId);
-        return brand;
+        return new Brand(brandId);
     }
 
     @Named("CategoryMapper")
-    default List<Category> brandMapper(List<Long> categories) {
-        List<Category> response = new ArrayList<>();
-        for (Long id : categories) {
-            Category cat = new Category(id, "default", "def");
-            response.add(cat);
-        }
-        return response;
-    }
+    default List<Category> categoryMapper(List<Long> categories) {
+        if (!categories.isEmpty()) {
+            List<Category> response = new ArrayList<>();
+            for (Long id : categories) {
+                Category cat = new Category(id, "defaultName", "defDescription");
+                response.add(cat);
+            }
+            return response;
+        } throw new EmptyFieldException(DomainConstants.FIELD_CATEGORIESID_ILLEGAL_ARGUMENT_MESSAGE);
 
+    }
 }
--- src/main/java/com/microservice/stockmicroservice/domain/model/Product.java
+++ src/main/java/com/microservice/stockmicroservice/domain/model/Product.java
@@ -1,6 +1,7 @@
 package com.microservice.stockmicroservice.domain.model;
 
 import com.microservice.stockmicroservice.domain.exceptions.EmptyFieldException;
+import com.microservice.stockmicroservice.domain.util.DomainConstants;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,30 +19,27 @@ public class Product {
     }
 
     public Product(Long id, String name, String description, Long quantity, BigDecimal price, Brand brand, List<Category> categoriesId) {
-            if (name == null || name.isEmpty()) {
-                throw new EmptyFieldException("The name cannot be null or empty");
-            }
-            if (description == null || description.isEmpty()) {
-                throw new EmptyFieldException("The description cannot be null or empty");
-            }
-            if (quantity == null) {
-                throw new EmptyFieldException("The quantity cannot be null or empty");
-            }
-            if (price == null) {
-                throw new EmptyFieldException("The price cannot be null or empty");
-            }
-            if (brand == null) {
-                throw new EmptyFieldException("The brand cannot be null or empty");
-            }
-            if (categoriesId == null || categoriesId.isEmpty()) {
-                throw new EmptyFieldException("The categoriesId list cannot be null or empty");
-            }
-            if (categoriesId.stream().distinct().count() != categoriesId.size()){
-            throw new EmptyFieldException("The categoriesId list has not content duplicate elements");
-            }
-            if ( categoriesId.size() > 3) {
-                throw new EmptyFieldException("The categoriesId list has not more than three elements");
-            }
+        if (name == null || name.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_NAME_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if (description == null || description.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_DESCRIPTION_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if (quantity == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_QUANTITY_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if (price == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_PRICE_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if (brand == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_BRAND_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if (categoriesId == null || categoriesId.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_CATEGORIESID_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if ( categoriesId.size() > 3) {
+                throw new EmptyFieldException(DomainConstants.FIELD_CATEGORIESID_MAXIMUM_ILLEGAL_ARGUMENT_MESSAGE);
+        }
 
         this.id = id;
         this.name = name;
@@ -51,6 +49,7 @@ public class Product {
         this.brand = brand;
         this.categoriesId = categoriesId;
     }
+
 
     public Long getId() {
         return id;
@@ -85,26 +84,46 @@ public class Product {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name == null || name.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_NAME_ILLEGAL_ARGUMENT_MESSAGE);
+        } this.name = name;
     }
 
     public void setDescription(String description) {
+        if (description == null || description.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_DESCRIPTION_ILLEGAL_ARGUMENT_MESSAGE);
+        }
         this.description = description;
     }
 
     public void setQuantity(Long quantity) {
+        if (quantity == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_QUANTITY_ILLEGAL_ARGUMENT_MESSAGE);
+        }
         this.quantity = quantity;
     }
 
     public void setPrice(BigDecimal price) {
+        if (price == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_PRICE_ILLEGAL_ARGUMENT_MESSAGE);
+        }
         this.price = price;
     }
 
     public void setBrand(Brand brand) {
+        if (brand == null) {
+            throw new EmptyFieldException(DomainConstants.FIELD_BRAND_ILLEGAL_ARGUMENT_MESSAGE);
+        }
         this.brand = brand;
     }
 
     public void setCategoriesId(List<Category> categoriesId) {
+        if (categoriesId == null || categoriesId.isEmpty()) {
+            throw new EmptyFieldException(DomainConstants.FIELD_CATEGORIESID_ILLEGAL_ARGUMENT_MESSAGE);
+        }
+        if ( categoriesId.size() > 3) {
+            throw new EmptyFieldException(DomainConstants.FIELD_CATEGORIESID_MAXIMUM_ILLEGAL_ARGUMENT_MESSAGE);
+        }
         this.categoriesId = categoriesId;
     }
 }
