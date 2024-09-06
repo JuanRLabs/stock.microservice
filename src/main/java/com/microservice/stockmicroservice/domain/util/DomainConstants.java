866410d5451ccba83bf848034dc4e7556863e31b
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-05 23:51:47 -0500

    se valida excepciones de modelo dominio por instanciacion

--- src/main/java/com/microservice/stockmicroservice/domain/util/DomainConstants.java
+++ src/main/java/com/microservice/stockmicroservice/domain/util/DomainConstants.java
@@ -13,8 +13,12 @@ public final class DomainConstants {
     public static final String FIELD_NAME_NULL_OR_ILLEGAL_MESSAGE = "Field 'name' cannot be null, too long or numeric";
     public static final String FIELD_DESCRIPTION_NULL_OR_ILLEGAL_MESSAGE = "Field 'description' cannot be null or too long";
     public static final String FIELD_PAGE_OR_SIZE_ILLEGAL_ARGUMENT_MESSAGE = "Field 'page' or 'size' cannot be negative value";
-    public static final String FIELD_SIZE_ILLEGAL_ARGUMENT_MESSAGE = "Field 'size' cannot be negative value";
-    public static final String FIELD_PAGE_ILLEGAL_ARGUMENT_MESSAGE = "Field 'page' cannot be negative value";
-
-
+    public static final String FIELD_NAME_ILLEGAL_ARGUMENT_MESSAGE = "The name cannot be null or empty";
+    public static final String FIELD_DESCRIPTION_ILLEGAL_ARGUMENT_MESSAGE = "The description cannot be null or empty";
+    public static final String FIELD_QUANTITY_ILLEGAL_ARGUMENT_MESSAGE = "The quantity cannot be null or empty";
+    public static final String FIELD_PRICE_ILLEGAL_ARGUMENT_MESSAGE = "The price cannot be null or empty";
+    public static final String FIELD_BRAND_ILLEGAL_ARGUMENT_MESSAGE = "The brand cannot be null or empty";
+    public static final String FIELD_CATEGORIESID_ILLEGAL_ARGUMENT_MESSAGE = "The categoriesId list cannot be null or empty";
+    public static final String FIELD_CATEGORIESID_CONTAIN_ILLEGAL_ARGUMENT_MESSAGE = "The categoriesId list has duplicate elements";
+    public static final String FIELD_CATEGORIESID_MAXIMUM_ILLEGAL_ARGUMENT_MESSAGE = "The categoriesId list has not more than three elements";
 }
