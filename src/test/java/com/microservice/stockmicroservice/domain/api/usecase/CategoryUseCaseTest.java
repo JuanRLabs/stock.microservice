64d19784e090a539934172e1fcb89aa76c49ceef
juanrodriguez9 <juansebastianrodriguez904@gmail.com>
2024-09-10 11:28:12 -0500

    fix: includes de junit - coverage / exclude

--- src/test/java/com/microservice/stockmicroservice/domain/api/usecase/CategoryUseCaseTest.java
+++ src/test/java/com/microservice/stockmicroservice/domain/api/usecase/CategoryUseCaseTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class CategoryUseCaseTest {
 
-    @Mock(lenient = true)
+    @Mock
     private ICategoryPersistencePort categoryPersistencePort;
 
     @InjectMocks
